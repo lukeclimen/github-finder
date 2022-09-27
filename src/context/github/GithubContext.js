0f35de5de0d19884786807f102ea626449b1c8be
Luke Climenhage <luke.climenhage@gmail.com>
2022-09-27 16:09:03 -0400

    Switched from useState to useReducer to allow action dispatching

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,27 +1,44 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer } from "react";
+import githubReducer from "./GithubReducer";
 
 const GithubContext = createContext();
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 const TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 export const GithubProvider = ({ children }) => {
-	const [users, setUsers] = useState([]);
-	const [isLoading, setIsLoading] = useState(true);
+	const initialState = {
+		users: [],
+		isLoading: true,
+	};
+
+	const [state, dispatch] = useReducer(
+		githubReducer,
+		initialState
+	);
 
 	const fetchUsers = async () => {
-		const response = await fetch(`${GITHUB_URL}/users`);
+		const response = await fetch(
+			`${GITHUB_URL}/users`
+			// {
+			// 	headers: {
+			// 		Authorization: `token ${TOKEN}`,
+			// 	},
+			// }
+		);
 		const data = await response.json();
 
-		setUsers(data);
-		setIsLoading(false);
+		dispatch({
+			type: "GET_USERS",
+			payload: data,
+		});
 	};
 
 	return (
 		<GithubContext.Provider
 			value={{
 				//Variables
-				users,
-				isLoading,
+				users: state.users,
+				isLoading: state.isLoading,
 				//Methods
 				fetchUsers,
 			}}
