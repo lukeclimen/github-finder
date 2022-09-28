534770f01358c15b40b631c0d71ac76e0cd231ea
Luke Climenhage <luke.climenhage@gmail.com>
2022-09-28 17:59:07 -0400

    Added more user data to the User page

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -77,6 +77,7 @@ function User() {
 					{/* Name and bio section */}
 					<div className='col-span-2'>
 						<div className='mb-6'>
+							{/* Name (Large) and bio */}
 							<h1 className='text-3xl card-title'>
 								{name}
 								{/* Badges for user type and whether or not they're hireable */}
@@ -90,6 +91,7 @@ function User() {
 								)}
 							</h1>
 							<p>{bio}</p>
+							{/* Go to GitHub profile button */}
 							<div className='mt-4 card-actions'>
 								<a
 									href={html_url}
@@ -100,6 +102,104 @@ function User() {
 									Visit Github Profile
 								</a>
 							</div>
+						</div>
+						{/* Location, Website and Twitter Links (if applicable) */}
+						<div className='w-full rounded-lg shadow-md bg-base-100 stats'>
+							{location && (
+								// Stat is a DaisyUI class
+								<div className='stat'>
+									<div className='stat-title td-md'>
+										Location
+									</div>
+									<div className='text-lg stat-value'>
+										{location}
+									</div>
+								</div>
+							)}
+							{blog && (
+								<div className='stat'>
+									<div className='stat-title td-md'>
+										Website
+									</div>
+									<div className='text-lg stat-value'>
+										<a
+											href={`https://${blog}`}
+											target='_blank'
+											rel='noreferrer'
+										>
+											{blog}
+										</a>
+									</div>
+								</div>
+							)}
+							{twitter_username && (
+								<div className='stat'>
+									<div className='stat-title td-md'>
+										Twitter
+									</div>
+									<div className='text-lg stat-value'>
+										<a
+											href={`https://twitter.com/${twitter_username}`}
+											target='_blank'
+											rel='noreferrer'
+										>
+											{
+												twitter_username
+											}
+										</a>
+									</div>
+								</div>
+							)}
+						</div>
+					</div>
+				</div>
+				<div className='w-full py-5 mb-6 rounded-lg shadow-md bg-base-100 stats'>
+					{/* Followers and purple 3 users icon */}
+					<div className='stat'>
+						<div className='stat-figure text-secondary'>
+							<FaUsers className='text-3xl md:texl-5xl' />
+						</div>
+						<div className='stat-title pr-5'>
+							Followers
+						</div>
+						<div className='stat-value pr-5 text-3xl md:text-4xl'>
+							{followers}
+						</div>
+					</div>
+					{/* Following and purple 2 users icon  */}
+					<div className='stat'>
+						<div className='stat-figure text-secondary'>
+							<FaUserFriends className='text-3xl md:texl-5xl' />
+						</div>
+						<div className='stat-title pr-5'>
+							Following
+						</div>
+						<div className='stat-value pr-5 text-3xl md:text-4xl'>
+							{following}
+						</div>
+					</div>
+					{/* Public repos and purple prism icon  */}
+					<div className='stat'>
+						<div className='stat-figure text-secondary'>
+							<FaCodepen className='text-3xl md:texl-5xl' />
+						</div>
+						<div className='stat-title pr-5'>
+							Public Repos
+						</div>
+						<div className='stat-value pr-5 text-3xl md:text-4xl'>
+							{public_repos}
+						</div>
+					</div>
+					{/* Public gists and purple store icon  */}
+					<div className='stat'>
+						<div className='stat-figure text-secondary'>
+							<FaStore className='text-3xl md:texl-5xl' />
+						</div>
+						<div className='stat-title pr-5'>
+							Public Gists
+						</div>
+						<div className='stat-value pr-5 text-3xl md:text-4xl'>
+							{public_gists}
 						</div>
 					</div>
 				</div>
